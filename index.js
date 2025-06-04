285373eb2c4b9f0887220b9b22c8ed19968e5aa4
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-06-04 15:34:47 +0500

    mongodb integrated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const notesRoutes = require('./routes/notesRoutes');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors({
@@ -16,11 +17,13 @@ app.use('/api/notes', notesRoutes);
 
 const startServer = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
     app.listen(process.env.PORT, () => {
       console.log(`Server running on http://localhost:${process.env.PORT}`);
     });
   } catch (err) {
-    console.error('Server error:', err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
